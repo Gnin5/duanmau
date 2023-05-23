8a6d52d759b8cc2e928e5662f61907321c8a49ff
Gnin5 <nfdai01@gmail.com>
2023-05-23 14:25:39 +0700

    update

---
+++ admin/category/add.php
@@ -0,0 +1,14 @@
+<form>
+    <div class="mb-3">
+        <label class="form-label">Mã Loại</label>
+        <input type="text" class="form-control" name="id_cate" disabled>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">tên loại</label>
+        <input type="password" class="form-control" name="name_cate">
+    </div>
+    <button type="submit" class="btn btn-primary">Thêm mới</button>
+    <button type="reset" class="btn btn-secondary">Nhập lại</button>
+    <a href="list.php"><input type="button" class="btn btn-info" value="Danh sách"></a>
+</form>
+</div>
---
+++ admin/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADMIN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="p-3 mb-2 bg-secondary text-white">
+            <h1>ADMIN</h1>
+        </div>
+        <nav class="p-3 mb-2 bg-secondary-subtle text-emphasis-secondary navbar-nav ">
+            <ul class="menu-main nav">
+                <li class="nav-item"><a class="nav-link active" href="index.php">Trang chủ</a></li>
+                <li class="nav-item"><a class="nav-link active" href="index.php?act=adddm">Danh mục</a></li>
+                <li class="nav-item"><a class="nav-link active" href="index.php?act=addsp">Sản phẩm</a></li>
+                <li class="nav-item"><a class="nav-link active" href="index.php?act=dskh">Khách hàng</a></li>
+                <li class="nav-item"><a class="nav-link active" href="index.php?act=dsbl">Bình luận</a></li>
+                <li class="nav-item"><a class="nav-link active" href="index.php?act=tk">Thống kê</a></li>
+            </ul>
+        </nav>
